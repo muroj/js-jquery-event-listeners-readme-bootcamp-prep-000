4b524ccb85ddf1a3a2cf4ddc05e8465b88fecb36
Joe Muro <jmuro0321@gmail.com>
2019-01-02 17:12:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  });  
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    console.log("HELLO FROM FRAME IT")
+    alert("HELLO FROM FRAME IT")
+    $(this).addClass("tasty")
+  });
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(key) {
+      if (key.which == 71) {
+        alert("You pressed the G key. Very wonderful job, good.")
+      }
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+  
+  $('img').addClass("tasty")
 
 });
